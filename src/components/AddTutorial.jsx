ead2a51a4d42608229cad31d2ac546ab6fca726d
Augustcode99 <burakodabasi99@yahoo.com>
2023-05-24 13:14:03 +0300

    mock data used

--- src/components/AddTutorial.jsx
+++ src/components/AddTutorial.jsx
@@ -4,20 +4,36 @@ const AddTutorial = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="container text-center mt-4">
       <h1 className="display-6 text-danger">Add a Movie</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label className="form-label" htmlFor="title">
+          <label
+            className="form-label"
+            htmlFor="title"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            value={title}
+          >
             Title
           </label>
           <input className="form-control" type="text" id="title" />
         </div>
         <div className="mb-3">
-          <label className="form-label" htmlFor="desc">
+          <label
+            className="form-label"
+            htmlFor="desc"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            value={description}
+          >
             Description
           </label>
           <input className="form-control" type="text" id="desc" />
--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 
 const TutorialList = () => {
-  return <div>TutorialList</div>;
+  const tutorials = [
+    { id: 1, title: "harry potter", description: "fantasy" },
+    { id: 2, title: "Jurrasic Park", description: "science fiction" },
+  ];
+
+  return (
+    <div className="container mt-4">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tutorials.map((item) => {
+            return (
+              <tr>
+                <td> {item.id} </td>
+                <td> {item.title} </td>
+                <td> {item.description} </td>
+                <td className="text-center text-nowrap">
+                  <FaEdit
+                    size={20}
+                    type="button"
+                    className="me-2 text-warning"
+                  />
+                  <AiFillDelete
+                    size={20}
+                    type="button"
+                    className="me-2 text-warning"
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TutorialList;
