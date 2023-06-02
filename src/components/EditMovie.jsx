3a9ff5cd368b0d0a972c16caf342423a247acb61
Augustcode99 <burakodabasi99@yahoo.com>
2023-06-02 19:51:07 +0300

    implemented static backdrop modal to custom button

---
+++ src/components/EditMovie.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { FaEdit } from "react-icons/fa";
+
+function EditMovie() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <FaEdit
+        size={20}
+        type="button"
+        className="me-2 text-warning"
+        onClick={handleShow}
+      />
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don not even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default EditMovie;
